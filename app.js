016d1554a7144efa285a1d40909bf04c722b7cbb
Abhay Singh <singhchirag188@gmail.com>
2024-04-02 11:47:34 +0530

    Basic Landing Page, Listing Page, and each property to display in detail page is ready.

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const path = require('path');
 const app = express();
 const Property = require('./models/property');
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
 connect()
 .then(() => {
     console.log('Connected to database');
@@ -17,16 +22,35 @@ async function connect(){
     await mongoose.connect('mongodb://localhost:27017/primestay');
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.urlencoded({ extended: true }));
 
 
-//Main Code
+/* to display home page */
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
+});
+
+/* to display all listing */
+app.get('/listings',(req,res)=>{
+    Property.find({})
+    .then((properties)=>{
+        res.render('listings', {properties});
+    })
+    .catch(err => {
+        res.render('error');
+    });
+});
+
+/* to display individual listing in detail*/
+app.get('/listings/:id',(req,res)=>{
+    let id = req.params.id;
+
+    Property.findById(id)
+    .then(card=>{
+        res.render('listing', {card});
+    })
+    .catch(err => {
+        res.render('error');
+    }); 
 });
 
 app.listen(3000, () => {

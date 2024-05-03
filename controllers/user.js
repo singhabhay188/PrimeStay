6399cf814d4e93fef293ca10d0f37439a9ef10e1
Abhay Singh <singhchirag188@gmail.com>
2024-05-03 19:23:12 +0530

    controller added for user route

---
+++ controllers/user.js
@@ -0,0 +1,52 @@
+/* Models */
+const User = require('../models/user');
+
+module.exports.displaySignup = (req, res) => {
+    res.render('signup', {message:req.flash('message')});
+};
+
+module.exports.signup = async (req, res) => {
+    try{
+        let {name,email,password} = req.body;
+        const user = new User({email,name,username:email});
+        await user.setPassword(password);   
+        await user.save();
+        req.login(user,(err)=>{
+            if(err) return next(err);
+            req.flash('message','Signed and Logged in Successfully');
+            res.redirect('/listing');
+        });
+    }
+    catch(err){
+        console.log(err.message);
+        if(err.code = 11000) req.flash('message', 'User already exists with such email');
+        else req.flash('message', 'Problem in registeration');
+        res.redirect('/user/signup');
+    }
+};
+
+module.exports.displayLogin = (req, res) => {
+    res.render('login',{message:req.flash('error')});
+};
+
+module.exports.afterlogin = (req, res) {
+    let url = res.locals.redirectUrl;
+    if(url){
+        console.log(res.locals?.redirectUrl);
+        return res.redirect(url);
+    }
+    req.flash('message','Logged in Successfully');
+    res.redirect('/listing');
+};
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err)=>{
+        if(err){
+            console.log(err);
+            next(err);
+        }
+
+        req.flash('message','Succesfully Logged Out');
+        res.redirect('/listing');
+    })
+};
--- routes/user.js
+++ routes/user.js
@@ -4,60 +4,22 @@ const passport = require('passport');
 
 const storeRedirectUrl = require('../middlewares/storeRedirectUrl');
 
-/* Models */
-const User = require('../models/user');
+/* Controllers */
+const userController = require('../controllers/user');
 
-router.get('/signup', (req, res) => {
-    res.render('signup', {message:req.flash('message')});
-});
+router.get('/signup', userController.displaySignup);
 
-router.post('/signup', async (req, res) => {
-    try{
-        let {name,email,password} = req.body;
-        const user = new User({email,name,username:email});
-        await user.setPassword(password);   
-        await user.save();
-        req.login(user,(err)=>{
-            if(err) return next(err);
-            req.flash('message','Signed and Logged in Successfully');
-            res.redirect('/listing');
-        });
-    }
-    catch(err){
-        console.log(err.message);
-        if(err.code = 11000) req.flash('message', 'User already exists with such email');
-        else req.flash('message', 'Problem in registeration');
-        res.redirect('/user/signup');
-    }
-});
+router.post('/signup', userController.signup);
 
 router.get('/login',(req,res)=>{
     res.render('login',{message:req.flash('error')});
 })
 
-router.post('/login', 
-  storeRedirectUrl,
-  passport.authenticate('local', { failureRedirect:'/user/login', failureFlash: true}),
-  function(req, res) {
-    let url = res.locals.redirectUrl;
-    if(url){
-        console.log(res.locals?.redirectUrl);
-        return res.redirect(url);
-    }
-    req.flash('message','Logged in Successfully');
-    res.redirect('/listing');
-});
+router.post('/login', storeRedirectUrl, 
+    passport.authenticate('local', { failureRedirect:'/user/login', failureFlash: true}), 
+    userController.afterlogin
+);
 
-router.get('/logout',(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            console.log(err);
-            next(err);
-        }
-
-        req.flash('message','Succesfully Logged Out');
-        res.redirect('/listing');
-    })
-})
+router.get('/logout', userController.logout);
 
 module.exports = router;
